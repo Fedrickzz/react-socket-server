ebf0819fa0c33931d2b020333180ab226e499bac
Fedrick <fedrickperez@outlook.es>
2025-03-15 08:58:32 +0100

    cors

--- models/server.js
+++ models/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
+const cors = require('cors');
+
 const Sockets = require('./sockets');
 
 class Server {
@@ -19,6 +21,9 @@ class Server {
   middlewares() {
     // public dir
     this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+    // CORS
+    this.use.app(cors());
   }
 
   configurarSockets() {
